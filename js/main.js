06388af52cadf35c77ab6208b21940c0c3a092f9
George Ils <v.vince@yandex.ru>
2018-07-24 23:18:00 +0300

    24.07.2018 navbar toggle

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ $(document).ready(function () {
   });
 });
 
+$( document ).ready( function () {
+  var url = window.location.href.substr( window.location.href.lastIndexOf( '/' ) + 1 );
+  $( '.nav-item a' ).each( function () {
+      if( $( this ).attr( 'href' ) === url || $( this ).attr( 'href' ) === '' ) {
+          $( this ).parent( 'li' ).addClass( 'active' );
+      }
+  });
+});
